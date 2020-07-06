50dba14dc947a0caa5e0f27c09720a5d6f62a141
Quang Đặng <quangdangfit@gmail.com>
2020-07-06 10:57:39 +0700

    add config

---
+++ config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+type Schema struct {
+	Database struct {
+		Host     string `mapstructure:"host"`
+		Port     int    `mapstructure:"port"`
+		Name     string `mapstructure:"name"`
+		User     string `mapstructure:"user"`
+		Password string `mapstructure:"password"`
+		Env      string `mapstructure:"env"`
+	} `mapstructure:"database"`
+}
+
+var Config Schema
+
+func init() {
+	config := viper.New()
+	config.SetConfigName("config")
+	config.AddConfigPath(".")          // Look for config in current directory
+	config.AddConfigPath("config/")    // Optionally look for config in the working directory.
+	config.AddConfigPath("../config/") // Look for config needed for tests.
+	config.AddConfigPath("../")        // Look for config needed for tests.
+
+	config.SetEnvKeyReplacer(strings.NewReplacer(".", "__"))
+	config.AutomaticEnv()
+
+	err := config.ReadInConfig() // Find and read the config file
+	if err != nil {              // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
+	err = config.Unmarshal(&Config)
+	if err != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+	// fmt.Printf("Current Config: %+v", Config)
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module goshop
 
 go 1.14
 
-require github.com/jinzhu/gorm v1.9.14
+require (
+	github.com/jinzhu/gorm v1.9.14
+	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/spf13/viper v1.7.0
+	gitlab.com/quangdangfit/gocommon v1.0.5 // indirect
+)
