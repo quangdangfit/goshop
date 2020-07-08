d489b1a65e177435ef98b99ca1734054c55550b8
Quang Đặng <quangdangfit@gmail.com>
2020-07-08 15:34:54 +0700

    split role to services and repositories

--- routers/admin.go
+++ routers/admin.go
@@ -3,14 +3,12 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"goshop/middlewares/roles"
-	"goshop/objects/role"
 )
 
 func Admin(e *gin.Engine) {
 	admin := e.Group("admin")
 	admin.Use(roles.CheckAdmin())
 	{
-		roleService := role.NewService()
 		admin.POST("/roles", roleService.CreateRole)
 	}
 }
