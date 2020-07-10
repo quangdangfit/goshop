6683d7852f3876f74b4e0c50688c510d1139e466
Quang Đặng <quangdangfit@gmail.com>
2020-07-10 17:38:00 +0700

    define models stock_quant and warehouse

---
+++ models/stockQuant.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"github.com/jinzhu/gorm"
+)
+
+type StockQuant struct {
+	UUID          string `json:"uuid" gorm:"unique;not null;index;primary_key"`
+	ProductUUID   string `json:"product_uuid" gorm:"not null;index"`
+	WarehouseUUID string `json:"warehouse_uuid" gorm:"not null;index"`
+	Quantity      uint   `json:"quantity"`
+
+	gorm.Model
+}
+
+func (s *StockQuant) BeforeCreate() error {
+	s.UUID = uuid.New().String()
+	return nil
+}
