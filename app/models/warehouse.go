da575a6f8a4a109cbd0e312f1d6af9f4ef016221
Quang Dang <quangdangfit@gmail.com>
2020-07-28 10:55:51 +0700

    remove warehouse schema

--- app/models/warehouse.go
+++ app/models/warehouse.go
@@ -19,20 +19,3 @@ func (w *Warehouse) BeforeCreate() error {
 	w.Active = true
 	return nil
 }
-
-type WarehouseResponse struct {
-	UUID   string `json:"uuid"`
-	Code   string `json:"code"`
-	Name   string `json:"name"`
-	Active bool   `json:"active"`
-}
-
-type WarehouseBodyRequest struct {
-	Code string `json:"code,omitempty" validate:"required"`
-	Name string `json:"name,omitempty" validate:"required"`
-}
-
-type WarehouseQueryRequest struct {
-	Code   string `json:"code,omitempty" form:"code"`
-	Active string `json:"active,omitempty" form:"active"`
-}
